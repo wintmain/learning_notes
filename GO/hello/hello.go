735b4eb3713e2c91cea620143a46ad9f8c38f32d
wintmain <wosintmain@gmail.com>
2025-11-29 10:22:06 +0800

    [lang][feat]Add some files

---
+++ GO/hello/hello.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: wintmain wosintmain@gmail.com
+ * @Date: 2025-07-06 12:05:29
+ * @LastEditors: wintmain wosintmain@gmail.com
+ * @LastEditTime: 2025-07-06 12:56:20
+ * @FilePath: /GO/hello.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+package main
+
+//func main() {
+//	fmt.Println(quote.Go())
+//}
